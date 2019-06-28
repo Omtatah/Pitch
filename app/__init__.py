20a7ec4c08241cd9d36e4810eaac7080b73a5d81
Omtatah <murtallahomtatah@gmail.com>
2019-06-28 14:54:32 +0300

    initialize flask extensions to the __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
@@ -7,12 +6,19 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
+simple = SimpleMDE()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+mail = Mail()
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 def create_app(config_state):
     app = Flask(__name__)
-    app.config.from_object(config_options[config_state])
+    app.config.from_object(config_options[config_name])
 
 
     bootstrap.init_app(app)
@@ -23,5 +29,12 @@ def create_app(config_state):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
+    # Initializing flask extensions
+    login_manager.init_app(app)
+    simple.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    db.init_app(app)
 
     return app
