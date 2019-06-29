1500a64a44c0d4e6dee1ca9eba2d991f73ca5030
Omtatah <murtallahomtatah@gmail.com>
2019-06-29 12:41:51 +0300

    defined the PostComment and PostPitch form (both from FLaskForms) classes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
+
+
+class PostCommentForm(FlaskForm):
+    comment = TextAreaField('Write a comment')
+    submit = SubmitField('Comment')
+
+
+class PostPitchForm(FlaskForm):
+    category = SelectField('Select the category', choices=[('Religion', 'Religion'), (
+        'General', 'General'), ('Educative', 'Educative'), ('Inspirational', 'Inspirational')])
+    text = TextAreaField('Type your pitch'),
+    submit = SubmitField('Post')
