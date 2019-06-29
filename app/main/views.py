bdb369deb8a67eb12754820e82d2ffb224ba1169
Omtatah <murtallahomtatah@gmail.com>
2019-06-29 12:45:26 +0300

    imported all flask dependancies and classes needed in the views.py page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,11 @@
-from flask import render_template, request, redirect, url_for,abort
+from flask import render_template,request,redirect,url_for,abort
 from . import main
+from ..models import Comment,User,Post
+from flask_login import login_required, current_user
 from .. import db
+from .forms import PostPitchForm,PostCommentForm
+import markdown2 
+
 
 @main.route('/')
 def index():
