c17d3a2e1e63edbf201936cce73040d68ebffb32
Omtatah <murtallahomtatah@gmail.com>
2019-06-29 21:08:46 +0300

    made a secret key and USER_MAIL URI

--- config.py
+++ config.py
@@ -1,15 +1,34 @@
+import os
 
 class Config:
-    pass
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://omtatah@localhost/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-class ProdConfig(Config):
-    pass
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://omtatah@localhost/pitches_test'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
+
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://omtatah@localhost/pitches'
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
-
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+export SECRET_KEY='12'
+export MAIL_USERNAME='omtatahmurtallah@gmail.com'
+export MAIL_PASSWORD='Murta11@h'
+
 
 python3.6 manage.py server
-
