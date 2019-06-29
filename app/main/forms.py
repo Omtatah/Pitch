18b4b14d3eb83cb2ce1d47ab7d343b974b296ae8
Omtatah <murtallahomtatah@gmail.com>
2019-06-29 13:41:30 +0300

    touch a base.html file

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
+from wtforms import StringField, TextAreaField, SubmitField, RadioField, SelectField
 
 
 class PostCommentForm(FlaskForm):
