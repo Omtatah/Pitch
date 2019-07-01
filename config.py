141b570e1390ec16d2110247c8e9a4704cb1ce67
Omtatah <murtallahomtatah@gmail.com>
2019-07-01 11:50:01 +0300

    change secret key

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('12')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/img'
     MAIL_SERVER = 'smtp.gmail.com'
